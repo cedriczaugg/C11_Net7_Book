542b4cf02112fdd794353ccd8e0d1f03e27f2219
Cedric Zaugg <cedric.zaugg@eta.ch>
2023-01-03 13:43:20 +0100

    Added trace listener.

--- source/Chapter04/Instrumenting/Program.cs
+++ source/Chapter04/Instrumenting/Program.cs
@@ -1,4 +1,14 @@
 ﻿using System.Diagnostics;
 
+string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "log.txt");
+
+Console.WriteLine($"Write log to: {logPath}");
+
+TextWriterTraceListener logFile = new(File.CreateText(logPath));
+
+Trace.Listeners.Add(logFile);
+
+Trace.AutoFlush = true;
+
 Debug.WriteLine("Debug says, I am watching!");
 Trace.WriteLine("Trace says: I am watching!");
