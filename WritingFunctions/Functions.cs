3668947d6b2eed9e6f4d86cc036b01fe40270dd9
Cedric Zaugg <cedric.zaugg@eta.ch>
2023-01-03 11:47:14 +0100

    Times tables.

---
+++ WritingFunctions/Functions.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="Program.cs">
+// Copyright (c)  ETA Manufacture Horlogère Suisse SA. All rights reserved.
+// </copyright>
+
+// namespace WritingFunctions;
+
+internal partial class Program
+{
+    static void TimesTables(byte number, byte size = 12)
+    {
+        WriteLine($"This is the {number} times table with {size} rows:");
+        for (int row = 1; row <= size; row++)
+        {
+            WriteLine($"{row} X {number} = {row * number}");
+        }
+        WriteLine();
+    }
+}
--- WritingFunctions/Program.cs
+++ WritingFunctions/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal partial class Program
+{
+    public static void Main(string[] args)
+    {
+        TimesTables(7, 15);
+    }
+}
