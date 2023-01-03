b0466c188b44dc256cb80bf01a8af5b705713bff
Cedric Zaugg <cedric.zaugg@eta.ch>
2023-01-03 13:16:09 +0100

    Changed solution structure.

--- WritingFunctions/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿internal partial class Program
-{
-    public static void Main(string[] args)
-    {
-        // TimesTables(7, 15);
-        decimal tacToPay = CalculateTax(amount: 254, twoLetterRegionCode: "CH");
-        WriteLine($"You must pay {tacToPay} in tax.");
-    }
-}
