c7c122b8956663c8eac815fc52956626456a17ca
Cedric Zaugg <cedric.zaugg@eta.ch>
2023-01-04 15:10:16 +0100

    Chapter 06 - Generics

---
+++ source/Chapter05/PacktLibraryNetStandard2/Records.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="Program.cs">
+// Copyright (c)  ETA Manufacture Horlogère Suisse SA. All rights reserved.
+// </copyright>
+
+using System.Runtime.CompilerServices;
+
+namespace Packt.Shared;
+
+public class ImmutablePerson
+{
+    // public string? FirstName { get; init; }
+    // public string? LastName { get; init; }
+}
---
+++ source/Chapter06/PeopleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using Packt.Shared;
+
+Person harry = new Person()
+{
+    Name = "Harry",
+    DateOfBirth = new(2001, 3, 25)
+};
+
+harry.WriteToConsole();
+
+// Non-generic Lookup collection
+System.Collections.Hashtable lookupObject = new ();
+lookupObject.Add(key: 1, value: "Alpha");
+lookupObject.Add(key: 2, value: "Beta");
+lookupObject.Add(key: 3, value: "Gammma");
+lookupObject.Add(key: harry, value: "Delta");
+
+int key = 2;
+
+WriteLine(format: "Key {0} has value: {1}",
+    arg0: key,
+    arg1: lookupObject[key]);
+
+WriteLine(format: "Key {0} has value: {1}",
+    arg0: harry,
+    arg1: lookupObject[harry]);
+
+// generic Lookup collection
+Dictionary<int, string> lookupIntString = new();
+lookupIntString.Add(key: 1, value: "Alpha");
+lookupIntString.Add(key: 2, value: "Beta");
+lookupIntString.Add(key: 3, value: "Gamma");
+lookupIntString.Add(key: 4, value: "Delta");
