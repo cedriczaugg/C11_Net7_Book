3a970ce364c9aab5524c5ae4baa955239bf1099e
Cedric Zaugg <cedric.zaugg@eta.ch>
2023-01-09 15:42:36 +0100

    Console app to publish.

---
+++ source/Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Xml.Linq;
+
+XDocument doc = new();
+
+string s1 = "Hello";
+String s2 = "World";
+
+WriteLine($"{s1} {s2}");
