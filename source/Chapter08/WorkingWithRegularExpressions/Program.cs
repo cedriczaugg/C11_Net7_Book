1098489e2936f734532a9101c6564bf762c7bc5f
Cedric Zaugg <cedric.zaugg@eta.ch>
2023-01-10 15:55:00 +0100

    Working with Regex.

---
+++ source/Chapter08/WorkingWithRegularExpressions/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+Write("Enter your age:");
+string input = ReadLine()!;
+
+Regex ageChecker = new (@"\d");
+
+if (ageChecker.IsMatch(input))
+{
+    WriteLine("Thank you.");
+}
+else
+{
+    WriteLine($"This is not a valid age: {input}");
+}
