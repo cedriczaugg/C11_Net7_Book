e7b5c481a62a67f37bd338fcce3adca22a529993
Cedric Zaugg <cedric.zaugg@eta.ch>
2023-01-11 15:37:27 +0100

    Working with ranges start.

---
+++ source/Chapter08/WorkingWithCollections/Program.Helpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithCollections
+{
+    internal partial class Program
+    {
+        static void Output(string title, IEnumerable<string> collection)
+        {
+            WriteLine(title);
+            foreach (string item in collection)
+            {
+                WriteLine($" {item}");
+            }
+        }
+
+        static void OutputPQ<TElement, TPriority>(string title,
+            IEnumerable<(TElement Element, TPriority Priority)> collection)
+        {
+            WriteLine(title);
+            foreach ((TElement, TPriority) item in collection)
+            {
+                WriteLine($" {item.Item1}: {item.Item2}");
+            }
+        }
+    }
+}
---
+++ source/Chapter08/WorkingWithRanges/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkingWithRanges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
