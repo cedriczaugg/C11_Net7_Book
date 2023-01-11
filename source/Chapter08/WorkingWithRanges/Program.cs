6ddc93e3c8adfabf720200a16d8e57083edf265e
Cedric Zaugg <cedric.zaugg@eta.ch>
2023-01-11 16:13:28 +0100

    Working with ranges start.

--- source/Chapter08/WorkingWithRanges/Program.cs
+++ source/Chapter08/WorkingWithRanges/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Samantha Jones";
+
+            // getting lengths of the first and last names
+
+            int lengthOfFirst = name.IndexOf(' ');
+            int lengthOfLast = name.Length - lengthOfFirst - 1;
+
+            // using substring
+
+            string firstName = name.Substring(0, lengthOfFirst);
+            string lastName = name.Substring(name.Length - lengthOfLast, lengthOfLast);
+
+            WriteLine($"First name: {firstName}, Last name: {lastName}");
+
+            // using spans
+
+            ReadOnlySpan<char> nameAsSpan = name.AsSpan();
+            ReadOnlySpan<char> lastNameSpan = lastName.AsSpan();
+            ReadOnlySpan<char> firstNameSpan = firstName.AsSpan();
+
+            WriteLine("First name: {0}, Last name: {1}",
+                arg0: firstNameSpan.ToString(),
+                arg1: lastNameSpan.ToString());
         }
     }
 }
