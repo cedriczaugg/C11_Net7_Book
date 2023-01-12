19733002b1b9116c21fd0ea286b9e12ff21ba227
Cedric Zaugg <cedric.zaugg@eta.ch>
2023-01-12 15:24:16 +0100

    Tar archives

---
+++ source/Chapter09/WorkingWithTarArchives/Program.Helpers.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="Program.cs">
+// Copyright (c)  ETA Manufacture Horlogère Suisse SA. All rights reserved.
+// </copyright>
+
+// namespace WorkingWithTarArchives;
+
+partial class Program
+{
+    static void WriteError(string message)
+    {
+        ConsoleColor previousColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.Red;
+        WriteLine($"FAIL: {message}");
+        ForegroundColor = previousColor;
+    }
+    
+    static void WriteWarning(string message)
+    {
+        ConsoleColor previousColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.DarkYellow;
+        WriteLine($"WARN: {message}");
+        ForegroundColor = previousColor;
+    }
+    
+    static void WriteInformation(string message)
+    {
+        ConsoleColor previousColor = ForegroundColor;
+        ForegroundColor = ConsoleColor.Blue;
+        WriteLine($"INFO: {message}");
+        ForegroundColor = previousColor;
+    }
+}
