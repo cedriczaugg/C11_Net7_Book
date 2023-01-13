4220204a08e52d5ca98a9c5b3ec9c90455311865
Cedric Zaugg <cedric.zaugg@eta.ch>
2023-01-13 11:16:06 +0100

    XML serialization.

---
+++ source/Chapter09/WorkingWithSerialization/Person.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="Program.cs">
+// Copyright (c)  ETA Manufacture Horlogère Suisse SA. All rights reserved.
+// </copyright>
+
+using System.Xml.Serialization;
+
+namespace Packt.Shared;
+
+public class Person
+{
+    public Person()
+    {
+    }
+
+    public Person(decimal initialSalary)
+    {
+        Salary = initialSalary;
+    }
+
+    [XmlAttribute("fname")]
+    public string? FirstName { get; set; }
+    
+    [XmlAttribute("lname")]
+    public string? LastName { get; set; }
+    
+    [XmlAttribute("dob")]
+    public DateTime DateOfBirth { get; set; }
+    public HashSet<Person> Children { get; set; }
+    public decimal Salary { get; set; }
+}
