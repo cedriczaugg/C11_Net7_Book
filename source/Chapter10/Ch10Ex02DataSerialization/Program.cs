03905268cdd6c4251ba706d0d8d16f4d272d96e6
Cedric Zaugg <cedric.zaugg@eta.ch>
2023-01-16 10:21:21 +0100

    Ch10 exercise get products and categories.

---
+++ source/Chapter10/Ch10Ex02DataSerialization/Program.cs
@@ -0,0 +1,15 @@
+﻿using Ch10Ex02DataSerialization.AutoGen;
+
+using (Northwind ctx = new Northwind())
+{
+    var categories = ctx.Categories;
+    foreach (var category in categories)
+    {
+        Console.WriteLine(category.CategoryName);
+        var products = ctx.Products.Where(p => p.Category == category);
+        foreach (var product in products)
+        {
+                Console.WriteLine(product.ProductName);
+        }
+    }
+}
